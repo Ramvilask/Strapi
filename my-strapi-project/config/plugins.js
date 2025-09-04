6be750588b7432777ac88d03b35b5ef2f0fc2cc1
Ramvilask <ramvilaskannaujiya@gmail.com>
2025-09-04 17:52:48 +0530

    Slug Config.js

--- my-strapi-project/config/plugins.js
+++ my-strapi-project/config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+module.exports = {
+  'slugify': {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: 'slug',   // the field where the slug will be stored
+          references: 'title', // which field to base the slug on
+        },
+      },
+    },
+  },
+};
