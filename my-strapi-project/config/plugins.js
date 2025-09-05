adce687120bc4d3412768816aba9310890feffe1
Ramvilask <ramvilaskannaujiya@gmail.com>
2025-09-05 17:13:54 +0530

    npm reinstalled

--- my-strapi-project/config/plugins.js
+++ my-strapi-project/config/plugins.js
@@ -1,14 +1,1 @@
-// module.exports = () => ({});
-module.exports = {
-  'slugify': {
-    enabled: true,
-    config: {
-      contentTypes: {
-        article: {
-          field: 'slug',   // the field where the slug will be stored
-          references: 'title', // which field to base the slug on
-        },
-      },
-    },
-  },
-};
+module.exports = () => ({});
